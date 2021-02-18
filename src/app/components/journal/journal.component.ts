0e4bdf4c8988adab471dc6e4e2fb085db4942250
Gvin <gvin.sim@gmail.com>
2021-02-18 14:24:48 +0200

    #12 Add journal auto scroll

--- src/app/components/journal/journal.component.ts
+++ src/app/components/journal/journal.component.ts
@@ -1,5 +1,5 @@
 import { DatePipe } from "@angular/common";
-import { Component, Input } from "@angular/core";
+import { AfterViewInit, Component, ElementRef, Input, QueryList, ViewChild, ViewChildren } from "@angular/core";
 import { GameJournal } from "src/app/data/game-journal";
 import { LocalizationService } from "src/app/services/game-localization/localization.service";
 
@@ -8,11 +8,28 @@ import { LocalizationService } from "src/app/services/game-localization/localiza
     templateUrl: './journal.component.html',
     styleUrls: ['./journal.component.scss']
 })
-export class SurvivorJournalComponent {
+export class SurvivorJournalComponent implements AfterViewInit {
+
+    @ViewChild('journalBox', {static: false}) journalBox?: ElementRef;
+    @ViewChildren('journalMessage') messageElements?: QueryList<any>;
+
     @Input()
     public model?: GameJournal;
 
+    private scrollContainer: any;
+    private isNearBottom: boolean;
+
     public constructor(private readonly localizationService: LocalizationService) {
+        this.isNearBottom = true;
+    }
+
+    public ngAfterViewInit(): void {
+        if (!this.journalBox || !this.messageElements) {
+            throw new Error('Journal box was not initialized.');
+        }
+
+        this.scrollContainer = this.journalBox.nativeElement;  
+        this.messageElements.changes.subscribe(_ => this.onItemElementsChanged());  
     }
     
     private getTimeStamp(timeStamp: Date): string {
@@ -27,5 +44,30 @@ export class SurvivorJournalComponent {
             const message = this.localizationService.translateString(record.message);
             return `[${timeStamp}] ${message}`;
         }) ?? [];
+    }
+
+    private onItemElementsChanged(): void {
+        if (this.isNearBottom) {
+            this.scrollToBottom();
+        }
+    }
+    
+    private scrollToBottom(): void {
+        this.scrollContainer.scroll({
+            top: this.scrollContainer.scrollHeight,
+            left: 0,
+            behavior: 'smooth'
+        });
+    }
+
+    public scrolled(event: any): void {
+        this.isNearBottom = this.isUserNearBottom();
+    }
+
+    private isUserNearBottom(): boolean {
+        const threshold = 150;
+        const position = this.scrollContainer.scrollTop + this.scrollContainer.offsetHeight;
+        const height = this.scrollContainer.scrollHeight;
+        return position > height - threshold;
     }
 }
